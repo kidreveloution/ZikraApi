d3c7432000ca3281e911c714d734758d047750c7
Aladdin <alyeldinshahin@gmail.com>
2023-11-22 00:33:08 -0500

    Added form submission and choosing location on map for submission

--- frontend/js/addMemory.js
+++ frontend/js/addMemory.js
@@ -2,6 +2,7 @@ document.getElementById('addMemoryBtn').addEventListener('click', function() {
     var icon = document.getElementById('icon');
     if (icon.classList.contains('fa-plus')) {
         datepicker.style.display = 'none'; 
+        activateMapClick();
         addMemDiv.style.removeProperty('display');
         icon.classList.remove('fa-plus');
         icon.classList.add('fa-minus');
@@ -9,7 +10,30 @@ document.getElementById('addMemoryBtn').addEventListener('click', function() {
     } else {
         datepicker.style.removeProperty('display');
         addMemDiv.style.display = 'none'; 
+        deactivateMapClick();
         icon.classList.remove('fa-minus');
         icon.classList.add('fa-plus');
     }
+});
+
+document.getElementById('submitMemoryBtn').addEventListener('click', function() {    
+    var memory ={
+        "title": document.getElementById('title').value,
+        "lat": lat,
+        "long": long,
+        "timestamp": document.getElementById('timestamp').value,
+        "link": document.getElementById('link').value,
+
+    }
+    console.log(memory)
+    
+    // $.ajax({
+    //     url: "../xxx/functions/email/",
+    //     type: "POST",
+    //     data: email,
+    //     contentType: "application/json",
+    //     success: function (data) {
+    //         callback(data);
+    //     }
+    // })
 });
--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,8 +1,11 @@
 src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
-
+var map;
+var clickListener; 
+var lat;
+var long;
 // Initialize Google Maps
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: { lat: 31.476737, lng: 34.4813380 }, // Set your own default coordinates
         styles: [
@@ -17,18 +20,32 @@ function initMap() {
         }
         ]
     });
-}
-document.addEventListener("DOMContentLoaded", function () {
-    var rangeInput = document.getElementById('rangeInput');
-    var thumb = document.getElementById('thumb');
 
-    rangeInput.addEventListener('input', function () {
-        var value = (this.value - this.min) / (this.max - this.min);
-        thumb.style.left = value * 100 + '%';
-    });
-});
+}
+// Function to activate map click event
+function activateMapClick() {
+    if (!clickListener) {
+        clickListener = map.addListener('click', function(e) {
+            lat = e.latLng.lat();
+            long = e.latLng.lng();
+            var inputField = document.getElementById('location');
+            inputField.innerHTML = lat + ', ' + long;
+        });
+    }
+}
+
+// Function to deactivate map click event
+function deactivateMapClick() {
+    if (clickListener) {
+        google.maps.event.removeListener(clickListener);
+        clickListener = null;
+    }
+}
+
 
 // Load the map
 window.onload = function() {
 initMap();
 };
+
+
