dea85af49fc8a8ee97029295bba0550fb179dcb8
Aladdin <alyeldinshahin@gmail.com>
2023-12-21 23:04:34 -0500

    Adding Amplify support

---
+++ amplifyApi/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "zikracontainer": {
+      "ApiName": "string",
+      "ClusterName": "string",
+      "ContainerNames": "string",
+      "PipelineName": "string",
+      "RootUrl": "string",
+      "ServiceName": "string"
+    }
+  }
+}
