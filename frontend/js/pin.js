bcae6f62194e74d24ebe728b9c0740649e5e101b
Aladdin <alyeldinshahin@gmail.com>
2024-01-03 00:44:08 -0500

    Removing uneeded Commented code

--- frontend/js/pin.js
+++ frontend/js/pin.js
@@ -23,63 +23,6 @@ function hideMarkers() {
   setMapOnAll(null);
 }
 
-// function buildContent(memory) {
-//     const content = document.createElement("div");
-//     content.style.borderColor = "#00000"; // Set the border color
-//     content.style.borderWidth = "2px"; // Set the border width
-//     content.style.borderStyle = "solid"; // Set the border style
-
-//     content.classList.add("memory");
-//     //memory = memory[0]
-//         content.innerHTML = `
-//         <div class="icon">
-//             <i aria-hidden="true" class="fa-solid ${memory['icon']}" title="${memory['title']}"></i>
-//         </div>
-//         <div class="details" style="position: relative;">
-//             <div class="link">
-//                 <a href="${memory['link']}" target="_blank">
-//                     <div class="title">${memory['title']}</div>
-//                 </a>
-//             </div>
-//             <div class="id" style="font-size: smaller;">
-//                 ${memory['id']}
-//             </div>
-//             <div class="close" style="position: flex; top: 0; right: 0; cursor: pointer;">
-//                 <i class="fa-solid fa-xmark"></i>
-//             </div>
-//         </div>
-//         `;
-
-//         content.innerHTML = `
-//         <div class="icon">
-//             <i aria-hidden="true" class="fa-solid ${memory['icon']}" title="${memory['title']}"></i>
-//             <span class="fa-sr-only">$</span>
-//         </div>
-//         <div class="details">
-//             <div class="price">$</div>
-//             <div class="address">$</div>
-//             <div class="features">
-//             <div>
-//                 <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
-//                 <span class="fa-sr-only">bedroom</span>
-//                 <span>$</span>
-//             </div>
-//             <div>
-//                 <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
-//                 <span class="fa-sr-only">bathroom</span>
-//                 <span>$</span>
-//             </div>
-//             <div>
-//                 <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
-//                 <span class="fa-sr-only">size</span>
-//                 <span>$ ft<sup>2</sup></span>
-//             </div>
-//             </div>
-//         </div>
-//         `;
-//     return content;
-// }
-
 function buildContent(memory) {
   const content = document.createElement("div");
 
@@ -122,16 +65,15 @@ function buildSharedContent(memories){
       <div class="details" >
           <div class="price">${memories.title}</div>
           <div class="address">${memories.titles}</div>
-          <div class="features">
-            ${linkContent}
-          </div>
+          <div class="features">${linkContent}</div>
+      </div>     
       `;
   return content;
 
 }
 
 function buildLinks(links){
-    // Assuming memories.links is an array of URLs
+  // Assuming memories.links is an array of URLs
 
   // Initialize an empty string to hold the HTML
   let htmlContent = '';
