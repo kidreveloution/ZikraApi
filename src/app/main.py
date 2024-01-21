1303080d173f781aac5a99df32d87849ec25ab65
Aladdin <alyeldinshahin@gmail.com>
2024-01-20 20:26:47 -0500

    Adding API Key handeling again

--- src/app/main.py
+++ src/app/main.py
@@ -38,14 +38,13 @@ class addEntry(BaseModel):
 
 async def get_api_key(api_key_header: str = Depends(api_key_header)):
     print("\n THIS IS API KEY ", api_key_header)
-    # if api_key_header == API_KEY:
-    #     return api_key_header
-    # else:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_401_UNAUTHORIZED,
-    #         detail="Invalid API Key",
-    #     )
-    pass
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
+        )
 
 @app.get("/")
 def root():
